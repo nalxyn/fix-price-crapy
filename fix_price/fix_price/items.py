e96ab398a2469dfb097a3694ce760ae87757c580
nalxyn <nalxyn800@gmail.com>
2024-10-08 23:38:36 -0700

    Add files via upload

---
+++ fix_price/fix_price/items.py
@@ -0,0 +1,36 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+from dataclasses import dataclass
+from enum import StrEnum
+
+
+class MarketingTags(StrEnum):
+    """Маркировка для товара"""
+    specialPrice = 'Спец цена'
+    isFresh = 'Свежий'
+    isNew = 'Новый'
+    isPromo = 'Промо'
+    adult = 'Для взрослых'
+    isSeason = 'Сезон'
+    isHit = 'Хит'
+    isQRMark = 'QR-маркер'
+    forbidden = 'forbidden'
+    unit = 'Единичный'
+
+
+@dataclass
+class FixPriceItem:
+    timestamp: int
+    RCP: str
+    url: str
+    title: str
+    marketing_tags: list[str]
+    brand: str
+    section: list[str]
+    price_data: dict[str, float | str]
+    stock: dict[str, bool | int]
+    assets: dict[str, str | list[str]]
+    metadata: dict[str, str]
+    variants: int
