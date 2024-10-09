b9f78c13dab29f702a20de265f7784398e883f9f
nalxyn <nalxyn800@gmail.com>
2024-10-08 23:37:59 -0700

    Create utils.py

---
+++ fix_price/fix_price/utils.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from typing import Iterable
+from fix_price.items import FixPriceItem
+
+def add_section(item: FixPriceItem, category: dict) -> None:
+    item.section.append(category['title'])
+    if category.get('parentCategory') is None:
+        return
+    else:
+        category = category['parentCategory']
+        return add_section(item, category)
+
+def get_urls() -> Iterable[str]:
+    base_dir = Path(__name__).absolute().parent.parent
+    with open(base_dir / 'urls', mode='r', encoding='utf-8') as file:
+        for url in file:
+            yield url
